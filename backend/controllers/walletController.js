5db955929e4b18f2b249f30ee4fd3053902b534d
fixorium <fixercoin.token@gmail.com>
2025-07-12 20:45:49 +0500

    Update walletController.js

--- backend/controllers/walletController.js
+++ backend/controllers/walletController.js
@@ -1,1 +1,10 @@
+const Wallet = require('../models/Wallet');
 
+exports.getBalance = async (req, res) => {
+  const wallet = await Wallet.findOne({ userId: req.user.id });
+  res.json({ balance: wallet?.balance || 0 });
+};
+
+exports.transferTokens = async (req, res) => {
+  // logic
+};
