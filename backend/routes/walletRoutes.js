0982f799024415ecd81ad2335dbe2cbe74e09985
fixorium <fixercoin.token@gmail.com>
2025-07-12 20:36:00 +0500

    Update walletRoutes.js

--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const walletController = require('../controllers/walletController');
 
+router.get('/balance', walletController.getBalance);
+router.post('/transfer', walletController.transferTokens);
+
+module.exports = router;
