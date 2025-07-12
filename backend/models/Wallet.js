76d5535d6c28da953dfa5df6db5087140a7aba33
fixorium <fixercoin.token@gmail.com>
2025-07-12 20:40:49 +0500

    Update Wallet.js

--- backend/models/Wallet.js
+++ backend/models/Wallet.js
@@ -1,1 +1,8 @@
+const mongoose = require('mongoose');
 
+const WalletSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  balance: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model('Wallet', WalletSchema);
