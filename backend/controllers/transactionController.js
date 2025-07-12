41ae7e9faf8e83526a35f56d21215ae77ad02501
fixorium <fixercoin.token@gmail.com>
2025-07-12 20:47:42 +0500

    Update transactionController.js

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -1,1 +1,6 @@
+const Transaction = require('../models/Transaction');
 
+exports.getTransactions = async (req, res) => {
+  const transactions = await Transaction.find({ userId: req.user.id }).sort({ date: -1 });
+  res.json(transactions);
+};
